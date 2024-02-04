b368c96553cffa505c146325c87b6f7a76bb4c0a
Lukasz Biernacki <lbiernacki@student.agh.edu.pl>
2024-02-04 10:21:39 +0100

    RandomWord class was added

---
+++ src/main/java/pl/edu/agh/hangman/InternalTestClass.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.hangman;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+public class InternalTestClass {
+
+    public static void main(String[] args) {
+        RandomWord lotteryWord = new RandomWord();
+
+        System.out.println(lotteryWord.randomWord());
+
+
+    }
+
+
+}
